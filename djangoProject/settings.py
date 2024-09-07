8cf4e67271ea1963f853c45b47e4967d4ae02cb2
persephone-wong <persephone.w315@gmail.com>
2024-09-07 09:41:17 -0700

    Add members folder

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoProject.urls'
+ROOT_URLCONF = 'members.urls'
 
 TEMPLATES = [
     {
--- members/urls.py
+++ members/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from .views import home
-from .views import settings
+from djangoProject.views import home
+from djangoProject.views import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
