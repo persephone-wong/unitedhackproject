8024936c6b0b7ea63fbf8a9884f71db31315b69f
Nicky Leung <nicky-leung@outlook.com>
2024-09-07 16:17:25 -0700

    Updated weather code to give user idea of weather

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -3,6 +3,37 @@ from django.http import JsonResponse
 from django.shortcuts import redirect
 import requests
 
+WEATHER_CODE_MAP = {
+    0: 'Clear sky',
+    1: 'Mainly clear',
+    2: 'Partly cloudy',
+    3: 'Overcast',
+    45: 'Fog',
+    48: 'Depositing rime fog',
+    51: 'Drizzle: Light',
+    53: 'Drizzle: Moderate',
+    55: 'Drizzle: Dense intensity',
+    56: 'Freezing Drizzle: Light',
+    57: 'Freezing Drizzle: Dense intensity',
+    61: 'Rain: Slight',
+    63: 'Rain: Moderate',
+    65: 'Rain: Heavy intensity',
+    66: 'Freezing Rain: Light',
+    67: 'Freezing Rain: Heavy intensity',
+    71: 'Snow fall: Slight',
+    73: 'Snow fall: Moderate',
+    75: 'Snow fall: Heavy intensity',
+    77: 'Snow grains',
+    80: 'Rain showers: Slight',
+    81: 'Rain showers: Moderate',
+    82: 'Rain showers: Violent',
+    85: 'Snow showers: Slight',
+    86: 'Snow showers: Heavy',
+    95: 'Thunderstorm: Slight or moderate',
+    96: 'Thunderstorm with slight hail',
+    99: 'Thunderstorm with heavy hail'
+}
+
 def home(request):
     if 'latitude' not in request.GET or 'longitude' not in request.GET:
         return render(request, 'home.html')
@@ -19,7 +50,7 @@ def home(request):
         'time': weather_data['current_weather']['time'],
         'date': '2023-10-01',
         'location': weather_data['timezone'] if weather_data else 'Unknown',
-        'weather': weather_data['current_weather']['weathercode'] if weather_data else 'N/A',
+        'weather': WEATHER_CODE_MAP.get(weather_data['current_weather']['weathercode']) if weather_data else 'N/A',
         'temperature': weather_data['current_weather']['temperature'] if weather_data else 'N/A',
         'work_estimate': '2 hours',
         'playlist': 'Top Hits'
