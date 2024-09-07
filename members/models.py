7340e87d8fa1b8051a786daa38d6b9389c34861c
persephone-wong <persephone.w315@gmail.com>
2024-09-07 13:15:11 -0700

    Add user model

--- members/models.py
+++ members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    departure_time = models.TimeField()
+    arrival_time = models.TimeField()
+    start_address = models.TextField()
+    arrival_address = models.TextField()
+
