fb96e16bcccfcdc2f4e0ab1f03f6b198ac2c5299
Nicky Leung <nicky-leung@outlook.com>
2024-09-07 23:53:16 -0700

    Fix spotify not functioning

--- djangoProject/spotify/spotify_auth.py
+++ djangoProject/spotify/spotify_auth.py
@@ -3,9 +3,11 @@ from spotipy import SpotifyOAuth
 import os
 
 def spotify_auth(request):
+
     sp_oauth = SpotifyOAuth( client_id = os.getenv('SPOTIFY_CLIENT_ID'),
                             client_secret = os.getenv('SPOTIFY_CLIENT_SECRET'),
                             redirect_uri='http://localhost:8000/spotify_callback/',
                             scope='user-read-playback-state user-modify-playback-state')
     auth_url = sp_oauth.get_authorize_url()
+    print(auth_url)
     return redirect(auth_url)
