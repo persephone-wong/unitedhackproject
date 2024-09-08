5dd806de85e994725883c0f2c31d8f853f0fa93b
Jarell <mjnvicencio@gmail.com>
2024-09-08 03:26:56 -0700

    fix bug

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render, redirect
-import requests
-import os
-from dotenv import load_dotenv
-import math
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import redirect
 import requests
 from datetime import datetime
 from djangoProject.home import home
-from djangoProject.functions import get_coordinates , add_trip
+
+
 
 def settings(request):
     return render(request, 'settings.html')
@@ -20,14 +17,17 @@ def clear_session_view(request):
 def traffic(request):
     return render(request, 'traffic.html')
 
+
 def weather(request):
     return render(request, 'weather.html')
+
 
 def road_work(request):
     return render(request, 'road_work.html')
 
+
 def accidents(request):
     return render(request, 'accidents.html')
 
-def home(request):
-    return render(request, 'home.html')
+def add_trip(request):
+    return render(request, 'add_trip.html')
