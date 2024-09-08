c5ebe816d440cfc263e3f1b157715da555481da2
Nicky Leung <nicky-leung@outlook.com>
2024-09-08 07:21:22 -0700

    Added gitignore

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
